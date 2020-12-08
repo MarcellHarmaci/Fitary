850e1090f018122f18d05737ebe6d93ee770a78c
Marcell Harmaci <harmaci.marci@gmail.com>
2020-12-08 16:07:45 +0100

    Added Firebase RTDB support

--- app/src/main/java/hu/bme/aut/fitary/MainActivity.kt
+++ app/src/main/java/hu/bme/aut/fitary/MainActivity.kt
@@ -2,10 +2,39 @@ package hu.bme.aut.fitary
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.firebase.database.FirebaseDatabase
+import hu.bme.aut.fitary.data.Workout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d("DEBUG_RTDB", "Call upload")
+        uploadWorkout()
+    }
+
+    private fun uploadWorkout() {
+        Log.d("DEBUG_RTDB", "Generate id")
+        val key = FirebaseDatabase.getInstance().reference.child("workouts").push().key
+        Log.d("DEBUG_RTDB", "id: " + key)
+        Log.d("DEBUG_RTDB", "Create Workout object")
+        val newWorkout = Workout(
+            "123",
+            "chest",
+            10,
+            "doit")
+        Log.d("DEBUG_RTDB", "workout: " + newWorkout.toString())
+        Log.d("DEBUG_RTDB", "Save object")
+        FirebaseDatabase.getInstance().reference
+            .child("workouts")
+            .child(key!!)
+            .setValue(newWorkout)
+            .addOnSuccessListener { Log.d("DEBUG_RTDB", "Save SUCCESSFUL") }
+            .addOnFailureListener { Log.d("DEBUG_RTDB", "Save FAILED") }
+            .addOnCanceledListener { Log.d("DEBUG_RTDB", "Save CANCELED") }
+            .addOnCompleteListener { Log.d("DEBUG_RTDB", "Save COMPLETED") }
+        Log.d("DEBUG_RTDB", "Firebase DB save returned")
     }
 }
---
+++ app/src/main/java/hu/bme/aut/fitary/data/Workout.kt
@@ -0,0 +1,8 @@
+package hu.bme.aut.fitary.data
+
+data class Workout (
+    var uid: String? = null,
+    var exercise: String? = null,
+    var repCount: Int? = null,
+    var comment: String? = null
+)
