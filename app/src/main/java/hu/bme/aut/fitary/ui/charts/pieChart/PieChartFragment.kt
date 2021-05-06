01885e80e13bc16397a6f6aea45141c7784b77f7
Marcell Harmaci <harmaci.marci@gmail.com>
2021-05-06 21:57:29 +0200

    Tweak PieChart settings

--- app/src/main/java/hu/bme/aut/fitary/ui/charts/pieChart/PieChartFragment.kt
+++ app/src/main/java/hu/bme/aut/fitary/ui/charts/pieChart/PieChartFragment.kt
@@ -1,12 +1,13 @@
 package hu.bme.aut.fitary.ui.charts.pieChart
 
+import android.graphics.Color
 import co.zsmb.rainbowcake.base.RainbowCakeFragment
 import co.zsmb.rainbowcake.dagger.getViewModelFromFactory
 import co.zsmb.rainbowcake.extensions.exhaustive
+import com.github.mikephil.charting.animation.Easing
 import com.github.mikephil.charting.data.PieData
 import com.github.mikephil.charting.data.PieDataSet
 import com.github.mikephil.charting.data.PieEntry
-import com.github.mikephil.charting.utils.ColorTemplate
 import hu.bme.aut.fitary.R
 import kotlinx.android.synthetic.main.fragment_chart_pie.*
 
@@ -21,8 +22,12 @@ class PieChartFragment : RainbowCakeFragment<PieChartViewState, PieChartViewMode
         pieChart.apply {
             isRotationEnabled = false
             description.isEnabled = false
-            setDrawEntryLabels(false)
-            legend.textSize = 14f
+            legend.isEnabled = false
+
+            setDrawEntryLabels(true)
+            setEntryLabelColor(Color.BLACK)
+            extraLeftOffset = 30f
+            extraRightOffset = 30f
         }
 
         viewModel.connectView()
@@ -46,24 +51,54 @@ class PieChartFragment : RainbowCakeFragment<PieChartViewState, PieChartViewMode
     private fun renderPieChart(exercises: List<PieChartPresenter.Exercise>) {
         val pieEntries: MutableList<PieEntry> = mutableListOf()
 
-        for (exercise in exercises) {
+        var otherScore = 0.0
+
+        for ((index, exercise) in exercises.sortedByDescending { it.sumOfScore }.withIndex()) {
+            if (index < 5) {
+                pieEntries.add(
+                    PieEntry(
+                        exercise.sumOfScore.toFloat(),
+                        exercise.name
+                    )
+                )
+            } else {
+                otherScore += exercise.sumOfScore
+            }
+        }
+
+        if (otherScore != 0.0) {
             pieEntries.add(
                 PieEntry(
-                    exercise.sumOfScore.toFloat(),
-                    exercise.name
+                    otherScore.toFloat(),
+                    "Other"
                 )
             )
         }
 
+        val colors = resources.getIntArray(R.array.beachColors).toMutableList()
+        if (otherScore == 0.0)
+            colors.removeLast()
+
         val dataSet = PieDataSet(pieEntries, "").apply {
-            setColors(ColorTemplate.JOYFUL_COLORS, 255)
+            setColors(colors.toIntArray(), 255)
             valueTextSize = 14f
+            valueTextColor = Color.BLACK
+
+            valueLinePart1Length = 0.5f
+            valueLinePart2Length = 0.1f
+
+            xValuePosition = PieDataSet.ValuePosition.OUTSIDE_SLICE
+            yValuePosition = PieDataSet.ValuePosition.OUTSIDE_SLICE
+        }
+
+        val pieData = PieData(dataSet).apply {
+            setValueTextColor(Color.BLACK)
         }
 
         pieChart.apply {
-            data = PieData(dataSet)
+            data = pieData
             notifyDataSetChanged()
-            invalidate()
+            animateXY(1500, 1500, Easing.EaseInOutCirc)
         }
     }
 
