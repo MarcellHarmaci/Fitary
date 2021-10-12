d2ad9041a4358343ba60214c98639b2911f096de
Marcell Harmaci <harmaci.marci@gmail.com>
2021-10-12 11:23:29 +0200

    Create user profile layout and add a new item to the navigation drawer

--- app/src/main/java/hu/bme/aut/fitary/MainActivity.kt
+++ app/src/main/java/hu/bme/aut/fitary/MainActivity.kt
@@ -46,7 +46,12 @@ class MainActivity : AppCompatActivity() {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
-            setOf(R.id.nav_social_workouts, R.id.nav_user_workouts, R.id.nav_charts),
+            setOf(
+                R.id.nav_social_workouts,
+                R.id.nav_user_workouts,
+                R.id.nav_charts,
+                R.id.nav_user_profile
+            ),
             drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
---
+++ app/src/main/java/hu/bme/aut/fitary/ui/userProfile/UserProfileFragment.kt
@@ -0,0 +1,4 @@
+package hu.bme.aut.fitary.ui.userProfile
+
+class UserProfileFragment {
+}
