5450d035b1ad463a561e1bc2f49cfe5ba9d65eae
Marcell Harmaci <harmaci.marci@gmail.com>
2021-11-01 16:42:35 +0100

    Implement rendering of workout details except the exercises

--- app/src/main/java/hu/bme/aut/fitary/ui/viewWorkout/ViewWorkoutFragment.kt
+++ app/src/main/java/hu/bme/aut/fitary/ui/viewWorkout/ViewWorkoutFragment.kt
@@ -1,10 +1,14 @@
 package hu.bme.aut.fitary.ui.viewWorkout
 
+import android.graphics.BitmapFactory
 import android.os.Bundle
+import android.view.View
 import co.zsmb.rainbowcake.base.RainbowCakeFragment
 import co.zsmb.rainbowcake.dagger.getViewModelFromFactory
 import co.zsmb.rainbowcake.extensions.exhaustive
+import com.bumptech.glide.Glide
 import hu.bme.aut.fitary.R
+import kotlinx.android.synthetic.main.fragment_view_workout.*
 
 class ViewWorkoutFragment : RainbowCakeFragment<ViewWorkoutViewState, ViewWorkoutViewModel>() {
 
@@ -21,13 +25,41 @@ class ViewWorkoutFragment : RainbowCakeFragment<ViewWorkoutViewState, ViewWorkou
     override fun render(viewState: ViewWorkoutViewState) {
         when (viewState) {
             is Loading -> {
-
+                pbLoading.visibility = View.VISIBLE
+                tvLoadFailed.visibility = View.GONE
             }
             is LoadingFailed -> {
-
+                pbLoading.visibility = View.GONE
+                tvLoadFailed.visibility = View.VISIBLE
             }
             is WorkoutLoaded -> {
+                pbLoading.visibility = View.GONE
+                tvLoadFailed.visibility = View.GONE
 
+                context?.let {
+                    if (viewState.workout.avatar != null) {
+                        val avatarAsBitmap = BitmapFactory.decodeByteArray(
+                            viewState.workout.avatar,
+                            0,
+                            viewState.workout.avatar.size
+                        )
+
+                        Glide.with(it)
+                            .asBitmap()
+                            .load(avatarAsBitmap)
+                            .circleCrop()
+                            .into(ivProfile)
+                    } else {
+                        Glide.with(it)
+                            .load(R.drawable.ic_launcher_background)
+                            .circleCrop()
+                            .into(ivProfile)
+                    }
+                }
+
+                tvTitle.text = viewState.workout.title
+                tvUsername.text = viewState.workout.author
+                tvScoreSum.text = getString(R.string.sum_equals, viewState.workout.score)
             }
         }.exhaustive
     }
