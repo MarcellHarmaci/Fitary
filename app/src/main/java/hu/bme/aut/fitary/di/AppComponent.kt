703b349c07b52c57286ef4fb6203a68e28dba3f7
MarcellHarmaci <harmaci.marci@gmail.com>
2021-11-30 22:46:32 +0100

    Customise app style (color theme)

--- app/src/main/java/hu/bme/aut/fitary/di/AppComponent.kt
+++ app/src/main/java/hu/bme/aut/fitary/di/AppComponent.kt
@@ -2,11 +2,8 @@ package hu.bme.aut.fitary.di
 
 import co.zsmb.rainbowcake.dagger.RainbowCakeComponent
 import co.zsmb.rainbowcake.dagger.RainbowCakeModule
-import hu.bme.aut.fitary.di.ApplicationModule
-import hu.bme.aut.fitary.di.ViewModelModule
 import dagger.Component
 import javax.inject.Singleton
-
 
 @Singleton
 @Component(
